b665f87410bc9154a4fc3122abbdcdb188c3220a
Eric2013 <zaizz0808@gmail.com>
2013-09-25 00:27:04 +0800

    add new files

---
+++ phpsso_server/caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => '39health',
+		'username' => 'root',
+		'password' => '860318',
+		'tablepre' => 'v9_sso_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		)
+);
+
+?>
